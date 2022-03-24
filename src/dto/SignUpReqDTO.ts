f92ac0c3a2d03831c5f675cddf694ad88cbb3847
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-03-24 01:18:07 +0100

    fix: cors

--- src/dto/SignUpReqDTO.ts
+++ src/dto/SignUpReqDTO.ts
@@ -11,5 +11,5 @@ export const SignUpReqDTOSchema: Joi.ObjectSchema = Joi.object({
 	email: Joi.string().email().required(),
 	name: Joi.string().required(),
 	username: Joi.string().required(),
-	password: Joi.string().min(8).required(),
+	password: Joi.string().min(1).required(),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express, { json } from 'express';
+import cors from 'cors';
 
 import { aboutRouter, runBroker } from '../src-ms';
 import { MS_EXPRESS_PORT } from './constants';
@@ -12,6 +13,7 @@ runBroker(channelProducers, channelConsumers);
 // Express
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 app.use('/users-service', aboutRouter);
